68e70851f6b390777e2c83873e86d8729a161a7e
Shingo OKAWA <aa-s_ookawa@corp.allabout.co.jp>
2015-09-25 14:13:06 +0900

    1-6-1

---
+++ Ruby/1-6-1.rb
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# PCCB 1-6-1
+
+class Problem
+  def initialize(a)
+    if a.kind_of?(Array) && a.all? {|x| x.is_a? Integer} then @a = a else @a = [] end
+    @a.sort! {|x, y| y <=> x}
+  end
+
+  def solve
+    for i in 0...@a.size
+      for j in (i+1)...@a.size
+        for k in (j+1)...@a.size
+          if @a[i] < @a[j] + @a[k]
+            print "answer: #{@a[i] + @a[j] + @a[k]} \n"
+            return
+          end
+        end
+      end
+    end
+    print "no answer found\n"
+  end
+
+  def debug
+    print @a
+    print "\n"
+  end
+end
+
+
+$a = []
+
+ARGV.each do |arg|
+  $a << arg.to_i
+end
+
+problem = Problem.new($a)
+problem.solve
