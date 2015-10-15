c9272a08d89c0a201cddc167c1a7dba4bc7afc6a
Shingo OKAWA <aa-s_ookawa@corp.allabout.co.jp>
2015-10-16 01:15:16 +0900

    2-1-3

--- Python/2-1-3.py
+++ Python/2-1-3.py
@@ -8,15 +8,16 @@ class EnumType(type):
         super(EnumType, clazz).__init__(name, bases, attributes)
         clazz._values = []
         for key, value in attributes.iteritems():
-            if is_enum_instance(key, value):
+            if EnumType._check_if_enum_instance(key, value):
                 instance = clazz(key, value)
-                setattr(clazz, key, item)
-                clazz._values.append(item)
+                setattr(clazz, key, instance)
+                clazz._values.append(instance)
 
     def __iter__(clazz):
-        return iter(clazz.values)
+        return iter(clazz._values)
 
-    def _is_enum_instance(clazz, key, value):
+    @staticmethod
+    def _check_if_enum_instance(key, value):
         return not hasattr(value, '__call__') and key.isupper()
 
 
@@ -28,14 +29,61 @@ class Enum(object):
         self.value = value
 
     def __repr__(self):
-        return repr(self.value)
+        return self.value if isinstance(self.value, str) else repr(self.value)
+
+    def __eq__(other):
+        return self.key == other.key and self.value == other.value if isinstance(other, self.__class__) else False
+
+
+class Solver(object):
+    class Cells(Enum):
+        WALL   = 'w'
+        PATH   = ' '
+        WALKED = '@'
+
+        @staticmethod
+        def parse(char):
+            for trail in Solver.Cells:
+                if char == trail.value:
+                    return trail
+            return Solver.Cells.WALL
+
+    class Trail(object):
+        def __init__(self, cell, previous=None):
+            self.cell = cell
+            self.previous = previous
+
+        def __repr__(self):
+            return repr(self.cell)
+
+    class Maze(object):
+        def __init__(self, data, rows, columns):
+            self.trails = []
+            self.rows = rows
+            self.columns = columns
+            for row in range(0, self.rows):
+                self.trails.append([])
+                for column in range(0, self.columns):
+                    try:
+                        self.trails[row].append(Solver.Trail(Solver.Cells.parse(data[row][column])))
+                    except IndexError:
+                        self.trails[row].append(Solver.Trail(Solver.Cells.WALL))
+
+        def __repr__(self):
+            result = ""
+            for row in range(0, self.rows):
+                for column in range(0, self.columns):
+                    result += repr(self.trails[row][column])
+                result += "\n"
+            return result
 
 
 def main(argv=None):
     if not argv:
-        argv = map(lambda line: line.rstrip('\n'), sys.argv.readlines())
+        argv = map(lambda line: line.rstrip('\n'), sys.stdin.readlines())
+        problem = Solver.Maze(argv, len(argv), len(max(argv, key=len)))
     if argv:
-        print argv
+        print problem
     else:
         print >> sys.stderr, "usage: python 2-1-3.py <map>"
 
